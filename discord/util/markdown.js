3b40a2a8c8ff07f42963a87dd1092d8a824d4235
bhyunnie <dev.bhyunnie@gmail.com>
2023-12-14 20:55:37 +0900

    공통 모듈 추가

---
+++ discord/util/markdown.js
@@ -0,0 +1,46 @@
+const bold = (content) => {
+  return `**${content}**`;
+};
+
+const italic = (content) => {
+  return `*${content}*`;
+};
+
+const boldAndItalic = (content) => {
+  return `***${content}***`;
+};
+
+const codeblock = (content) => {
+  return "```" + content + "```";
+};
+
+const hideAndShow = (content) => {
+  return `||${content}||`;
+};
+
+const blank = () => {
+  return "\u200b";
+};
+
+const quote = (content) => {
+  return `> ${content}`;
+};
+
+const list = (contentArray) => {
+  return contentArray
+    .map((content) => {
+      return `- ${content}`;
+    })
+    .join("\n");
+};
+
+export default {
+  bold,
+  italic,
+  boldAndItalic,
+  codeblock,
+  blank,
+  hideAndShow,
+  quote,
+  list,
+};
---
+++ util/path.js
@@ -0,0 +1,5 @@
+import path from "path";
+
+const dirname = path.resolve();
+
+export { dirname };
