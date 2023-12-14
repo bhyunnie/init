d9baa37e4c8621cf98dc6d6201864de31c6a0254
bhyunnie <dev.bhyunnie@gmail.com>
2023-12-14 20:32:07 +0900

    모듈 분리

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-import { Events } from "discord.js";
 import { makeDBConnection } from "./db/db.js";
-import client from "./discord/config/client.js";
-import { clog, cerror } from "./util/log.js";
+import { clog } from "./util/log.js";
 import discord from "./discord/index.js";
 
 // TODO
@@ -18,30 +16,3 @@ await makeDBConnection();
 // Discord Command Setting
 discord.attachEventListeners();
 discord.connectBot();
-
-client.on(Events.InteractionCreate, async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-  const command = interaction.client.commands.get(interaction.commandName);
-
-  if (!command) {
-    cerror(`${interaction.commandName} 이런 명령어는 존재하지 않아요 😢`);
-    return;
-  }
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    cerror(error);
-    if (interaction.replied || interaction.deferred) {
-      await interaction.followUp({
-        content: "There was an error while executing this command!",
-        ephemeral: true,
-      });
-    } else {
-      await interaction.reply({
-        content: "There was an error while executing this command!",
-        ephemeral: true,
-      });
-    }
-  }
-});
